c736af6bcdd1dc65d1e1e6d660cca30c781b0194
prejuk <93695329+prejuk@users.noreply.github.com>
2023-12-01 19:18:32 -0800

    Add files via upload

---
+++ War Card Game/War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Preju Kanuparthy on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
